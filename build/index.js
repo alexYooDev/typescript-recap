787172124e77808bd9d9e6ad70e4e823bcab79a1
d_flex <hiyoo95@gmail.com>
2022-09-05 03:48:19 +0900

    complete implementing block chain with ts

--- build/index.js
+++ build/index.js
@@ -1,11 +1,1 @@
-var hello = function () { return 'Hi!'; };
-var Block = /** @class */ (function () {
-    function Block(data) {
-        this.data = data;
-        this.data = 'Initial Data';
-    }
-    Block.prototype.printData = function () {
-        return this.data;
-    };
-    return Block;
-}());
+"use strict";
